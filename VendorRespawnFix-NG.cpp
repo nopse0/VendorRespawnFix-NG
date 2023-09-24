b27d602f5f183dadd53ea1ce286a2e2560ede750
nirme <nirmer0@googlemail.com>
2023-09-24 16:17:08 +0200

    vcpkg integration test

---
+++ VendorRespawnFix-NG.cpp
@@ -0,0 +1,12 @@
+﻿// VendorRespawnFix-NG.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "VendorRespawnFix-NG.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
